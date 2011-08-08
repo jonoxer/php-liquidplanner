a6844c1a1d61b6bef9b036489c6e4784da7d4de3
Jonathan Oxer <jon@oxer.com.au>
2011-08-08 15:51:47 +1000

    Added update_estimate method

--- liquidplanner.php
+++ liquidplanner.php
@@ -26,7 +26,7 @@ class LiquidPlanner
 	 * description
 	 * parent_id
 	 */
-	public function create_task($task)
+	public function create_task(array $task)
 	{
 		$encoded_task = json_encode(
 			array('task' =>
@@ -48,6 +48,29 @@ class LiquidPlanner
 		curl_setopt($conn, CURLOPT_ENCODING, "");                                        // Prevent GZIP compression of response from LP
 		curl_setopt($conn, CURLOPT_USERPWD, $this->email.":".$this->password);           // Authenticate
 		curl_setopt($conn, CURLOPT_URL, $this->serviceurl . '/tasks');                   // Set the service URL
+		$response = curl_exec($conn);
+		curl_close($conn);
+
+		/* The response is JSON, so decode it and return the result as an array */
+		return(json_decode($response, TRUE));
+	}
+
+	/**
+	 */
+	public function update_estimate($taskid, array $data)
+	{
+		$encoded_task = json_encode($data);
+
+		/* Set up the CURL object and execute it */
+		$conn = curl_init();
+		curl_setopt($conn, CURLOPT_HEADER, FALSE);                                       // Suppress display of the response header
+		curl_setopt($conn, CURLOPT_HTTPHEADER, array("Content-Type: application/json")); // Must submit as JSON
+		curl_setopt($conn, CURLOPT_RETURNTRANSFER, TRUE);                                // Return result as a string
+		curl_setopt($conn, CURLOPT_POST, TRUE);                                          // Submit data as an HTTP POST
+		curl_setopt($conn, CURLOPT_POSTFIELDS, $encoded_task);                           // Set the POST field values
+		curl_setopt($conn, CURLOPT_ENCODING, "");                                        // Prevent GZIP compression of response from LP
+		curl_setopt($conn, CURLOPT_USERPWD, $this->email.":".$this->password);           // Authenticate
+		curl_setopt($conn, CURLOPT_URL, $this->serviceurl.'/tasks/'.$taskid.'/track_time'); // Set the service URL
 		$response = curl_exec($conn);
 		curl_close($conn);
 
