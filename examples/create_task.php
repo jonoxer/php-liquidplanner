a132fc611c17a556ba1097038b71260d0f47d2bf
Jonathan Oxer <jon@oxer.com.au>
2011-08-08 14:16:34 +1000

    Initial version of library

---
+++ examples/create_task.php
@@ -0,0 +1,27 @@
+#!/usr/bin/php
+<?php
+/**
+ * @author Jonathan Oxer <jon.oxer@ivt.com.au>
+ * @version 2011-08-08
+ *
+ * This example is designed to be run from the command line using
+ * PHP-CLI, and is a minimal example of using the php-liquidplanner
+ * library to create a new task in your workspace. You must configure
+ * it to use your own workspace ID, email address, and Liquid Planner
+ * password, and set a parent_id value of one of your existing
+ * projects or project folders.
+ */
+require_once("../liquidplanner.php");
+
+$task['name']        = "My LP ticket";
+$task['parent_id']   = 123456;
+$task['description'] = "Description of my LP ticket";
+
+/* Create an instance of the Liquid Planner object */
+$lp = new LiquidPlanner("12345", "you@example.com", "yourLPpassword");
+
+/* Create a new ticket in Liquid Planner */
+$response = $lp->create_task($task);
+echo "ID of the new task: ".$response['id']."\n";
+
+exit;
---
+++ liquidplanner.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @author Jonathan Oxer <jon.oxer@ivt.com.au>
+ * @version 2011-08-08
+ */
+
+class LiquidPlanner
+{
+
+	private $email = '';
+	private $password = '';
+	private $serviceurl = '';
+
+	/**
+	 * Constructor
+	 */
+	public function __construct($workspaceID, $email, $password)
+	{
+		$this->email      = $email;
+		$this->password   = $password;
+		$this->serviceurl = "https://app.liquidplanner.com/api/workspaces/" . $workspaceID;
+	}
+	
+	/**
+	 * $ticket['name']
+	 * description
+	 * parent_id
+	 */
+	public function create_task($task)
+	{
+		$encoded_task = json_encode(
+			array('task' =>
+				array(
+					'name'        => $task['name'],
+					'description' => $task['description'],
+					'parent_id'   => $task['parent_id']
+				)
+			)
+		);
+
+		/* Set up the CURL object and execute it */
+		$conn = curl_init();
+		curl_setopt($conn, CURLOPT_HEADER, FALSE);                                       // Suppress display of the response header
+		curl_setopt($conn, CURLOPT_HTTPHEADER, array("Content-Type: application/json")); // Must submit as JSON
+		curl_setopt($conn, CURLOPT_RETURNTRANSFER, TRUE);                                // Return result as a string
+		curl_setopt($conn, CURLOPT_POST, TRUE);                                          // Submit data as an HTTP POST
+		curl_setopt($conn, CURLOPT_POSTFIELDS, $encoded_task);                           // Set the POST field values
+		curl_setopt($conn, CURLOPT_ENCODING, "");                                        // Prevent GZIP compression of response from LP
+		curl_setopt($conn, CURLOPT_USERPWD, $this->email.":".$this->password);           // Authenticate
+		curl_setopt($conn, CURLOPT_URL, $this->serviceurl . '/tasks');                   // Set the service URL
+		$response = curl_exec($conn);
+		curl_close($conn);
+
+		/* The response is JSON, so decode it and return the result as an array */
+		return(json_decode($response, TRUE));
+	}
+}
