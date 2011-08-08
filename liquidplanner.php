503c37bca69dd70e1a903ff8d436a28e22ceb421
Jonathan Oxer <jon@oxer.com.au>
2011-08-08 16:40:42 +1000

    Generalised CURL call to simplify public methods

--- liquidplanner.php
+++ liquidplanner.php
@@ -18,7 +18,7 @@ class LiquidPlanner
 	{
 		$this->email      = $email;
 		$this->password   = $password;
-		$this->serviceurl = "https://app.liquidplanner.com/api/workspaces/" . $workspaceID;
+		$this->serviceurl = "https://app.liquidplanner.com/api/workspaces/".$workspaceID;
 	}
 	
 	/**
@@ -28,7 +28,7 @@ class LiquidPlanner
 	 */
 	public function create_task(array $task)
 	{
-		$encoded_task = json_encode(
+		$encodedTask = json_encode(
 			array('task' =>
 				array(
 					'name'        => $task['name'],
@@ -37,40 +37,36 @@ class LiquidPlanner
 				)
 			)
 		);
-
-		/* Set up the CURL object and execute it */
-		$conn = curl_init();
-		curl_setopt($conn, CURLOPT_HEADER, FALSE);                                       // Suppress display of the response header
-		curl_setopt($conn, CURLOPT_HTTPHEADER, array("Content-Type: application/json")); // Must submit as JSON
-		curl_setopt($conn, CURLOPT_RETURNTRANSFER, TRUE);                                // Return result as a string
-		curl_setopt($conn, CURLOPT_POST, TRUE);                                          // Submit data as an HTTP POST
-		curl_setopt($conn, CURLOPT_POSTFIELDS, $encoded_task);                           // Set the POST field values
-		curl_setopt($conn, CURLOPT_ENCODING, "");                                        // Prevent GZIP compression of response from LP
-		curl_setopt($conn, CURLOPT_USERPWD, $this->email.":".$this->password);           // Authenticate
-		curl_setopt($conn, CURLOPT_URL, $this->serviceurl . '/tasks');                   // Set the service URL
-		$response = curl_exec($conn);
-		curl_close($conn);
-
-		/* The response is JSON, so decode it and return the result as an array */
-		return(json_decode($response, TRUE));
+		$url = $this->serviceurl.'/tasks';
+		$response = $this->execute_connection($url, $encodedTask);
+		return($response);
 	}
 
 	/**
 	 */
 	public function update_estimate($taskid, array $data)
 	{
-		$encoded_task = json_encode($data);
+		$encodedTask = json_encode($data);
+		$url = $this->serviceurl.'/tasks/'.$taskid.'/track_time';
+		$response = $this->execute_connection($url, $encodedTask);
+		return($response);
+	}
 
+	/**
+	 *
+	 */
+	private function execute_connection($url, $encodedTask)
+	{
 		/* Set up the CURL object and execute it */
 		$conn = curl_init();
 		curl_setopt($conn, CURLOPT_HEADER, FALSE);                                       // Suppress display of the response header
 		curl_setopt($conn, CURLOPT_HTTPHEADER, array("Content-Type: application/json")); // Must submit as JSON
 		curl_setopt($conn, CURLOPT_RETURNTRANSFER, TRUE);                                // Return result as a string
 		curl_setopt($conn, CURLOPT_POST, TRUE);                                          // Submit data as an HTTP POST
-		curl_setopt($conn, CURLOPT_POSTFIELDS, $encoded_task);                           // Set the POST field values
+		curl_setopt($conn, CURLOPT_POSTFIELDS, $encodedTask);                            // Set the POST field values
 		curl_setopt($conn, CURLOPT_ENCODING, "");                                        // Prevent GZIP compression of response from LP
 		curl_setopt($conn, CURLOPT_USERPWD, $this->email.":".$this->password);           // Authenticate
-		curl_setopt($conn, CURLOPT_URL, $this->serviceurl.'/tasks/'.$taskid.'/track_time'); // Set the service URL
+		curl_setopt($conn, CURLOPT_URL, $url);                                           // Set the service URL
 		$response = curl_exec($conn);
 		curl_close($conn);
 
